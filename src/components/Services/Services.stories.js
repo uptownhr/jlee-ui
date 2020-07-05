1105669deaa7e1e9e0297aeed0f8353e7934be32
James Lee <uptownhr@gmail.com>
2020-07-04 20:42:48 -0700

    added services component

---
+++ src/components/Services/Services.stories.js
@@ -0,0 +1,11 @@
+import Services from "./Services";
+import "../../assets/tailwind.css";
+
+export default {
+  title: "Services"
+};
+
+export const Default = () => ({
+  components: { Services },
+  template: `<Services />`
+});
---
+++ src/components/Services/Services.vue
@@ -0,0 +1,168 @@
+<template>
+  <div id="services" class="py-24 bg-white">
+    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-center">
+        <p
+          class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase"
+        >
+          Services
+        </p>
+        <h3
+          class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+        >
+          Multiple angles of learning
+        </h3>
+        <p
+          class="mt-8 mb-16 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto"
+        >
+          Coaching from Mentor, Peer and Alumni support, through Pair
+          Programming, chat and PR Review.
+        </p>
+      </div>
+
+      <div class="mt-10">
+        <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+          <li>
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div
+                  class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                >
+                  <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <h4 class="text-lg leading-6 font-medium text-gray-900">
+                  Pair Programming
+                </h4>
+                <p class="mt-2 text-base leading-6 text-gray-500">
+                  It is extremely important that we build a relationship and
+                  learn how to communicate and work with each other. Through
+                  pair programming, I can provide immediate feedback and work on
+                  your challenges together. Whether it’s questions on
+                  documentation or code that you’re writing for work let’s solve
+                  them together.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="mt-10 md:mt-0">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div
+                  class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                >
+                  <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <h4 class="text-lg leading-6 font-medium text-gray-900">
+                  Peer Programming
+                </h4>
+                <p class="mt-2 text-base leading-6 text-gray-500">
+                  Mentor is one part of mentorship. I will pair you with another
+                  Mentees also learning Vue. Pairing with a mentee expands the
+                  different types of challenges and provide a cooperative
+                  environment to problem solve.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="mt-10 md:mt-0">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div
+                  class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                >
+                  <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <h4 class="text-lg leading-6 font-medium text-gray-900">
+                  Invitation to Private Slack Server
+                </h4>
+                <p class="mt-2 text-base leading-6 text-gray-500">
+                  Chat is a great tool for handling asynchronous idea sharing
+                  and discussions. Throughout the day, week, we can explore your
+                  questions and curiosities together. We are building a network
+                  of Alumnis that have asked and solved the challenges focused
+                  in our stack.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="mt-10 md:mt-0">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div
+                  class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                >
+                  <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <h4 class="text-lg leading-6 font-medium text-gray-900">
+                  Pull Request Review
+                </h4>
+                <p class="mt-2 text-base leading-6 text-gray-500">
+                  As you become comfortable with Vue and start making commits
+                  towards projects, the PR process is a great place to gain
+                  additional insights. The context provided by a project tailors
+                  the design decisions and feedback that can be provided.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
